bab0961feb444bd108b74588cac5666412295352
Sheikh Jami Jashim <jami6bd@gmail.com>
2023-10-06 11:17:00 +0600

    merge conflicts

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Banner from './Banner';
+import PopularClasses from './PopularClasses/PopularClasses';
+import FeaturedInstructors from './FeaturedInstructors/FeaturedInstructors';
+import SpecialServices from './Dialog/SpecialServices';
+import Pricing from './Pricing/Pricing';
+import Instragram from './Instragram/Instragram';
+import { useContext } from 'react';
+import { ThemeContext } from '../../provider/ThemeProvider';
+import WhyWeLearn from './WhyWeLearn/WhyWeLearn';
+import CompanyCourses from '../CompanyCourses/CompanyCourses';
+import PeopleSay from './PeopleSay/PeopleSay';
+import Subscribe from './Subscribe/Subscribe';
+
+
+const Home = () => {
+      const { theme, toggleTheme } = useContext(ThemeContext);
+      return (
+            <div >
+
+                  <Banner></Banner>
+
+                  <SpecialServices></SpecialServices>
+                  <PopularClasses />
+                  <WhyWeLearn />
+                  <CompanyCourses />
+                  <FeaturedInstructors></FeaturedInstructors>
+                  <Pricing></Pricing>
+                  <div className=' bg-slate-100 shadow-lg mb-24 py-16'><Instragram></Instragram></div>
+                  <PeopleSay />
+                  <Subscribe />
+
+            </div>
+      );
+};
+
+export default Home;
--- src/pages/Login/SocialLogin/SocialLogin.jsx
+++ src/pages/Login/SocialLogin/SocialLogin.jsx
@@ -5,57 +5,45 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 const SocialLogin = () => {
       const { googleSignIn } = useContext(AuthContext);
-      const navigate= useNavigate();
+      const navigate = useNavigate();
       const location = useLocation();
-      const [error,setError]=useState('')
+      const [error, setError] = useState('')
 
-      const from= location.state?.from?.pathname || '/';
+      const from = location.state?.from?.pathname || '/';
 
 
       const handlegooglesignIn = () => {
             setError('');
             googleSignIn()
-            .then(result=>{
-                  const user = result.user;
-                  const createUser={
-                        name:user.displayName,
-                        photo:user.photoURL,
-                        email:user.email
-                  }
-                  // console.log(result.user);
-                  LoadData(createUser);
-                 
-            }).catch(error=>setError(error.message))
-       }
-       const LoadData=(user)=>{
-            axios.post('https://language-learning-school-server.vercel.app/users',user)
-            .then(res=>{
-                  // console.log(res);
-                  navigate(from,{replace:true})
-            })
-       }
+                  .then(result => {
+                        const user = result.user;
+                        const createUser = {
+                              name: user.displayName,
+                              photo: user.photoURL,
+                              email: user.email
+                        }
+                        // console.log(result.user);
+                        LoadData(createUser);
+
+                  }).catch(error => setError(error.message))
+      }
+      const LoadData = (user) => {
+            axios.post('https://language-learning-school-server.vercel.app/users', user)
+                  .then(res => {
+                        // console.log(res);
+                        navigate(from, { replace: true })
+                  })
+      }
       return (
             <div className=' flex gap-4'>
                   {
-                        error&&<p>{error}</p>
+                        error && <p>{error}</p>
                   }
-<<<<<<< HEAD
-                  <button className="btn btn-circle btn-outline">
-                        <FaFacebook size={40} />
-                  </button>
+
                   <button onClick={handlegooglesignIn} className="btn btn-circle btn-outline">
                         <FaGoogle size={40} />
                   </button>
-                  <button className="btn btn-circle btn-outline">
-                        <FaGithub size={40} />
-                  </button>
-=======
-                  
-                  <button onClick={handlegooglesignIn} className="btn btn-circle btn-outline">
-                        <FaGoogle size={40} />
-                  </button>
-                  
->>>>>>> 79984570798fda715b0dfb7958c68a4f5439e821
+
             </div>
       );
 };
